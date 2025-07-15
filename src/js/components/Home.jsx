c0ea59583daa822539f00b0ff4d1ab93082b15e9
monkeyjp <43521145+monkeyjp@users.noreply.github.com>
2025-07-15 20:10:49 +0000

    component and styles

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,93 @@
-import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [personajes, setPersonajes] = useState([])
+	const [newPersonaje, setNewPersonaje] = useState({
+		nombre: "",
+		frase: "",
+		imagen: ""
+	})
+	const [showAlert, setShowAlert] = useState(false);
+
+	const handleInputsChange = (e) => {
+		setNewPersonaje({ ...newPersonaje, [e.target.name]: e.target.value })
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (!newPersonaje.nombre || !newPersonaje.frase || !newPersonaje.imagen) {
+			setShowAlert(true);
+			setTimeout(() => setShowAlert(false), 2000);
+			return;
+		}
+		setPersonajes([...personajes, newPersonaje])
+		setNewPersonaje({
+			nombre: "",
+			frase: "",
+			imagen: ""
+		})
+
+
+	}
+	return (
+		<div className="container pt-4">
+			<h2 className="text-center text-light"><img style={{ height: "40px" }} src="https://upload.wikimedia.org/wikipedia/commons/d/d4/One_Ring_Blender_Render.png" alt="" /> Crea tu personaje de El Señor de los Anillos</h2>
+
+			{showAlert && (
+				<div className="alert alert-warning" role="alert">
+					Todos los campos son obligatorios
+				</div>
+			)}
+
+			<form className="mb-5" onSubmit={handleSubmit}>
+				<input
+					type="text"
+					className="form-control mb-2"
+					placeholder="Nombre"
+					name="nombre"
+					value={newPersonaje.nombre}
+					onChange={handleInputsChange}
+				/>
+				<input
+					type="text"
+					className="form-control mb-2"
+					placeholder="Frase"
+					name="frase"
+					value={newPersonaje.frase}
+					onChange={handleInputsChange}
+				/>
+				<input
+					type="text"
+					className="form-control mb-2"
+					placeholder="URL de la imagen"
+					value={newPersonaje.imagen}
+					name="imagen"
+					onChange={handleInputsChange}
+				/>
+				<button className="btn btn-success w-100">Agregar Personaje</button>
+			</form>
+
+			<div className="row">
+				{personajes.map((p, index) => (
+					<div className="col-md-4 mb-4" key={index}>
+						<div className="card h-100 shadow">
+							<img
+								src={p.imagen}
+								className="card-img-top"
+								alt={p.nombre}
+								style={{ height: "300px", objectFit: "cover" }}
+							/>
+							<div className="card-body">
+								<h5 className="card-title">{p.nombre}</h5>
+								<p className="card-text fst-italic">"{p.frase}"</p>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
